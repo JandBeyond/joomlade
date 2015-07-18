8e81936a9ab9b6d4433533a6297df96d11616eee
Ufuk Avcu <info@joomla-agentur.de>
2015-07-18 14:42:40 +0200

    Slideshow Progress

--- src/templates/joomlade/html/mod_articles_news/header-slideshow.php
+++ src/templates/joomlade/html/mod_articles_news/header-slideshow.php
@@ -1,43 +1,38 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  mod_articles_news
- *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
+* @package Joomla.Site
+* @subpackage mod_articles_news
+*
+* @copyright Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+* @license GNU General Public License version 2 or later; see LICENSE.txt
+*/
 defined('_JEXEC') or die;
 ?>
-
-    
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <?php foreach ($list as $key=>$item): ?>
-    <li data-target="#carousel-example-generic" data-slide-to="<?php echo $key; ?>" <?php echo ($key == '0' ? 'class="active"' : ''); ?>></li>
-    <?php endforeach; ?>
-  </ol>
-    
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <?php foreach ($list as $key=>$item): ?>
+        <li data-target="#carousel-example-generic" data-slide-to="<?php echo $key; ?>" <?php echo ($key == '0' ? 'class="active"' : ''); ?>></li>
+        <?php endforeach; ?>
+    </ol>
     <div class="carousel-inner" role="listbox">
         <?php foreach ($list as $key=>$item): ?>
         <div class="item <?php echo ($key == '0' ? 'active' : ''); ?>">
-            <?php $images = json_decode($item->images);?>
-            <img class="slide" src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="slide">
-            <div class="carousel-caption">
-                <?php echo $item->introtext; ?>
-            </div>  
+        <?php $images = json_decode($item->images);?>
+        <img class="slide" src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="slide">
+        <div class="carousel-caption">
+        <?php echo $item->introtext; ?>
+        </div>
         </div>
         <?php endforeach; ?>
     </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
     <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
-  </a>
+    </a>
 </div>
