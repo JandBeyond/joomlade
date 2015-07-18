4fac1eac02d7b476af5a6346f2eee40fa32f7745
Ufuk Avcu <info@joomla-agentur.de>
2015-07-18 17:31:38 +0200

    Carousel Blog div correction

--- src/templates/joomlade/html/com_content/article/carousel-blog.php
+++ src/templates/joomlade/html/com_content/article/carousel-blog.php
@@ -17,8 +17,8 @@ JHtml::_('behavior.caption');
 <div class="carouselblog <?php echo $this->pageclass_sfx; ?> col-md-12" itemscope itemtype="http://schema.org/Blog">
 	<div class="carousel carouselbloginner slide" id="myCarouselblogcarousel">
 		<div class="carousel-inner">
-	<?php $leadingcount = 0; ?>
-	<?php if (!empty($this->lead_items)) : ?>
+			
+			<?php $leadingcount = 0; ?>
 			<?php foreach ($this->lead_items as &$item) : ?>
 				<div class="item <?php ($leadingcount == 0 ? 'active' : '') ?>" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
 					<div class="col-md-4">
@@ -30,8 +30,8 @@ JHtml::_('behavior.caption');
 				</div>
 				<?php $leadingcount++; ?>
 			<?php endforeach; ?>
+		
 		</div>
-	<?php endif; ?>
 		<a class="left carousel-control" href="#myCarouselblogcarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
 		<a class="right carousel-control" href="#myCarouselblogcarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
 	</div>
