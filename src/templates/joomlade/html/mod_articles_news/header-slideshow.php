9213346c9d47527cd96f15a37c9eacd62a720283
Ufuk Avcu <info@joomla-agentur.de>
2015-07-18 13:37:04 +0200

    Slideshow Progress

--- src/templates/joomlade/html/mod_articles_news/header-slideshow.php
+++ src/templates/joomlade/html/mod_articles_news/header-slideshow.php
@@ -14,37 +14,26 @@ defined('_JEXEC') or die;
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      <?php foreach ($list as $key=>$item): ?>
+    <li data-target="#carousel-example-generic" data-slide-to="<?php echo $key; ?>" class="<?php if($key == 0) ? 'active':''; ?>"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="<?php echo $key; ?>"></li>
+
+      <?php enforeach; ?>
   </ol>
     
-    
-    <?php 
-    foreach ($list as $item){
-        echo '<pre>';
-        print_r ($item->introtext);
-        print_r ($item->images);
-        echo '</pre>';
-    }
-    ?>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="..." alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
+    <div class="carousel-inner" role="listbox">
+        <?php foreach ($list as $key=>$item): ?>
+            <div class="item" class="<?php if($key == 0) ? 'active':''; ?>">
+            <?php $images = json_decode($item->images);?>
+            <?php echo htmlspecialchars($images->image_intro); ?>
+            
+        <div class="carousel-caption">
+            <?php echo $item->introtext; ?>
+        </div>
+            
+        </div>
+        <?php enforeach; ?>
     </div>
-    <div class="item">
-      <img src="..." alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    ...
-  </div>
 
   <!-- Controls -->
   <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
