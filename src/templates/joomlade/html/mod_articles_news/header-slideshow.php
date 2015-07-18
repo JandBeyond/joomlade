0662abb75b1aef50af0ab9b3e785df2102860433
Ufuk Avcu <info@joomla-agentur.de>
2015-07-18 13:55:53 +0200

    Slideshow Progress

--- src/templates/joomlade/html/mod_articles_news/header-slideshow.php
+++ src/templates/joomlade/html/mod_articles_news/header-slideshow.php
@@ -15,19 +15,18 @@ defined('_JEXEC') or die;
   <!-- Indicators -->
   <ol class="carousel-indicators">
     <?php foreach ($list as $key=>$item): ?>
-    <li data-target="#carousel-example-generic" data-slide-to="<?php echo $key; ?>" class="<?php echo ($key == '0' ? 'active' : ''); ?>"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="<?php echo $key; ?>" <?php echo ($key == '0' ? 'class="active"' : ''); ?>></li>
     <?php endforeach; ?>
   </ol>
     
     <div class="carousel-inner" role="listbox">
         <?php foreach ($list as $key=>$item): ?>
-            <div class="item" class="<?php echo $key; echo ($key == '0' ? 'active' : ''); ?>">
+        <div class="item <?php echo $key; echo ($key == '0' ? 'active' : ''); ?>">
             <?php $images = json_decode($item->images);?>
             <img class="slide" src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="slide">
-        <div class="carousel-caption">
-            <?php echo $item->introtext; ?>
-        </div>
-            
+            <div class="carousel-caption">
+                <?php echo $item->introtext; ?>
+            </div>  
         </div>
         <?php endforeach; ?>
     </div>
