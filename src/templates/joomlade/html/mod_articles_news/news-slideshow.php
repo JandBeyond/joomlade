831e5b7b4fc2819a9ba03e32095b4f3e7256907a
David Jardin <d.jardin@djumla.de>
2015-09-13 13:55:08 +0200

    Added content limitation

--- src/templates/joomlade/html/mod_articles_news/news-slideshow.php
+++ src/templates/joomlade/html/mod_articles_news/news-slideshow.php
@@ -25,7 +25,7 @@ defined('_JEXEC') or die;
 
                         <h4><?php echo $item->title; ?></h4>
 
-                        <?php echo $item->introtext; ?>
+                        <?php echo substr(strip_tags($item->introtext), 0, 180); ?>...
 
                         <?php $tagLayout = new JLayoutFile('joomla.content.tags'); ?>
                         <?php echo $tagLayout->render($item->tags->itemTags); ?>
