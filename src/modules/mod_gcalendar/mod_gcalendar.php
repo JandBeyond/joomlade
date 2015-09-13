268e5246c95fbfdefae30a779ab503a9ebdb792b
David Neukirchen <david.neukirchen@gmail.com>
2015-09-13 16:56:56 +0200

    Fixed wrong default layout

--- src/modules/mod_gcalendar/mod_gcalendar.php
+++ src/modules/mod_gcalendar/mod_gcalendar.php
@@ -15,4 +15,4 @@ $gcalenderHelper = new ModGCalendarHelper($params);
 // Get the next events
 $events = $gcalenderHelper->nextEvents();
 
-require JModuleHelper::getLayoutPath('mod_gcalendar', $params->get('layout', 'list'));
+require JModuleHelper::getLayoutPath('mod_gcalendar', $params->get('layout', 'default'));
