8c60987f560b1fec0f6e6c1bc1e89eb9fbc83dba
David Jardin <d.jardin@djumla.de>
2015-09-19 16:22:13 +0200

    Added GA script

--- src/templates/joomlade/index.php
+++ src/templates/joomlade/index.php
@@ -167,6 +167,17 @@ $doc->addScript($tpath.'/js/joomlade.js');
         </footer>
       </div>
   </div>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-55288205-1', 'auto');
+    ga('set', 'anonymizeIp', true);
+    ga('require', 'displayfeatures');
+    ga('send', 'pageview');
+  </script>
 </body>
 <jdoc:include type="modules" name="debug" />
 </html>
