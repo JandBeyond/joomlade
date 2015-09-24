af0222bb9aa01c68462dd393a38e3b1621ce554b
Guido De Gobbis <guido.de.gobbis@joomtools.de>
2015-09-24 11:09:09 +0200

    #53 Content > Joomla > Links - show in categories view

---
+++ src/templates/joomlade/html/com_weblinks/categories/default_childs.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_weblinks
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$class = ' class="first"'; ?>
+
+<ul>
+    <?php foreach ($this->weblinks as $id => $child) :
+
+        if (!isset($this->weblinks[$id + 1]))
+        {
+            $class = ' class="last"';
+        }
+        ?>
+        <li<?php echo $class; ?>>
+            <?php $class = ''; ?>
+            <span class="item-title"><a href="<?php echo JRoute::_(WeblinksHelperRoute::getCategoryRoute($child->id)); ?>">
+                    <?php echo $this->escape($child->title); ?></a>
+            </span>
+        </li>
+
+    <?php endforeach; ?>
+</ul>
--- src/templates/joomlade/html/com_weblinks/categories/default_items.php
+++ src/templates/joomlade/html/com_weblinks/categories/default_items.php
@@ -14,122 +14,88 @@ JHtml::_('bootstrap.tooltip');
 $class = ' class="first"';
 
 if (count($this->items[$this->parent->id]) > 0 && $this->maxLevelcat != 0) :
-?>
-	<?php foreach($this->items[$this->parent->id] as $id => $item) : ?>
-		<?php
+    ?>
+    <?php foreach ($this->items[$this->parent->id] as $id => $item) : ?>
+    <?php
 
+    // Get an instance of the generic articles model
+    $model = JModelLegacy::getInstance('Category', 'WeblinksModel', array('ignore_request' => true));
 
+    // Set application parameters in model
+    $app       = JFactory::getApplication();
+    $appParams = $app->getParams();
+    $model->setState('params', $appParams);
 
-	
-	// Get an instance of the generic articles model
-	$model = JModelLegacy::getInstance('Category', 'WeblinksModel', array('ignore_request' => true));
+    // Set the filters based on the module params
+    $model->setState('list.start', 0);
+    $model->setState('list.limit', (int) $appParams->get('history_limit', 5));
 
-	// Set application parameters in model
-	$app = JFactory::getApplication();
-	$appParams = $app->getParams();
-	$model->setState('params', $appParams);
+    $model->setState('filter.state', 1);
+    $model->setState('filter.publish_date', true);
 
-	// Set the filters based on the module params
-	$model->setState('list.start', 0);
-	$model->setState('list.limit', (int) $appParams->get('history_limit', 5));
+    // Access filter
+    $access = !JComponentHelper::getParams('com_weblinks')->get('show_noauth');
+    $model->setState('filter.access', $access);
 
-	$model->setState('filter.state', 1);
-	$model->setState('filter.publish_date', true);
+    $catid = (int) $item->id;
+    $model->setState('category.id', $catid);
 
-	// Access filter
-	$access = !JComponentHelper::getParams('com_weblinks')->get('show_noauth');
-	$model->setState('filter.access', $access);
+    // Create query object
+    $db    = JFactory::getDbo();
+    $query = $db->getQuery(true);
 
-	$catid	= (int) $item->id;
-	$model->setState('category.id', $catid);
+    $case_when1 = ' CASE WHEN ';
+    $case_when1 .= $query->charLength('a.alias', '!=', '0');
+    $case_when1 .= ' THEN ';
+    $a_id = $query->castAsChar('a.id');
+    $case_when1 .= $query->concatenate(array($a_id, 'a.alias'), ':');
+    $case_when1 .= ' ELSE ';
+    $case_when1 .= $a_id . ' END as slug';
 
-	// Create query object
-	$db = JFactory::getDbo();
-	$query = $db->getQuery(true);
+    $case_when2 = ' CASE WHEN ';
+    $case_when2 .= $query->charLength('c.alias', '!=', '0');
+    $case_when2 .= ' THEN ';
+    $c_id = $query->castAsChar('c.id');
+    $case_when2 .= $query->concatenate(array($c_id, 'c.alias'), ':');
+    $case_when2 .= ' ELSE ';
+    $case_when2 .= $c_id . ' END as catslug';
 
-	$case_when1 = ' CASE WHEN ';
-	$case_when1 .= $query->charLength('a.alias', '!=', '0');
-	$case_when1 .= ' THEN ';
-	$a_id = $query->castAsChar('a.id');
-	$case_when1 .= $query->concatenate(array($a_id, 'a.alias'), ':');
-	$case_when1 .= ' ELSE ';
-	$case_when1 .= $a_id . ' END as slug';
+    $model->setState(
+        'list.select',
+        'a.*, c.published AS c_published,' . $case_when1 . ',' . $case_when2 . ',' . 'DATE_FORMAT(a.created, "%Y-%m-%d") AS created'
+    );
 
-	$case_when2 = ' CASE WHEN ';
-	$case_when2 .= $query->charLength('c.alias', '!=', '0');
-	$case_when2 .= ' THEN ';
-	$c_id = $query->castAsChar('c.id');
-	$case_when2 .= $query->concatenate(array($c_id, 'c.alias'), ':');
-	$case_when2 .= ' ELSE ';
-	$case_when2 .= $c_id . ' END as catslug';
+    $model->setState('filter.c.published', 1);
 
-	$model->setState(
-		'list.select',
-		'a.*, c.published AS c_published,' . $case_when1 . ',' . $case_when2 . ',' . 'DATE_FORMAT(a.created, "%Y-%m-%d") AS created'
-	);
+    // Filter by language
+    $model->setState('filter.language', $app->getLanguageFilter());
 
-	$model->setState('filter.c.published', 1);
+    $childs = $model->getItems();
 
-	// Filter by language
-	$model->setState('filter.language', $app->getLanguageFilter());
-
-
-	$_childs = $model->getItems();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		if ($this->params->get('show_empty_categories_cat') || $item->numitems || count($item->getChildren())) :
-		if (!isset($this->items[$this->parent->id][$id + 1]))
-		{
-			$class = ' class="last"';
-		}
-		?>
-		<div <?php echo $class; ?> >
-		<?php $class = ''; ?>
-			<h3 class="page-header item-title">
-				<a href="<?php echo JRoute::_(WeblinksHelperRoute::getCategoryRoute($item->id));?>">
-					<?php echo $this->escape($item->title); ?></a>
-					<?php if ($this->params->get('show_cat_num_articles_cat') == 1) :?>
-						<span class="badge badge-info tip hasTooltip" title="<?php echo JHtml::tooltipText('COM_WEBLINKS_NUM_ITEMS'); ?>">
+    if ($this->params->get('show_empty_categories_cat') || $item->numitems || count($item->getChildren())) :
+        if (!isset($this->items[$this->parent->id][$id + 1]))
+        {
+            $class = ' class="last"';
+        }
+        ?>
+        <div <?php echo $class; ?> >
+            <?php $class = ''; ?>
+            <h3 class="page-header item-title">
+                    <?php echo $this->escape($item->title); ?>
+                <?php if ($this->params->get('show_cat_num_articles_cat') == 1) : ?>
+                    <span class="badge badge-info tip hasTooltip" title="<?php echo JHtml::tooltipText('COM_WEBLINKS_NUM_ITEMS'); ?>">
 							<?php echo $item->numitems; ?>
 						</span>
-					<?php endif; ?>
-					<?php if (count($item->getChildren()) > 0 && $this->maxLevelcat > 1) : ?>
-						<a href="#category-<?php echo $item->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right"><span class="icon-plus"></span></a>
-					<?php endif;?>
-				</h3>
-				<?php if ($this->params->get('show_subcat_desc_cat') == 1) :?>
-					<?php if ($item->description) : ?>
-						<div class="category-desc">
-				<?php echo JHtml::_('content.prepare', $item->description, '', 'com_weblinks.categories'); ?>
-						</div>
-					<?php endif; ?>
-				<?php endif; ?>
-
-				<?php if (count($item->getChildren()) > 0 && $this->maxLevelcat > 1) :?>
-					<div class="collapse fade" id="category-<?php echo $item->id;?>">
-						<?php
-						$this->items[$item->id] = $item->getChildren();
-						$this->parent = $item;
-						$this->maxLevelcat--;
-						echo $this->loadTemplate('items');
-						$this->parent = $item->getParent();
-						$this->maxLevelcat++;
-						?>
-					</div>
-				<?php endif; ?>
-			</div>
-		<?php endif; ?>
-	<?php endforeach; ?>
+                <?php endif; ?>
+            </h3>
+            <?php
+        if(count($childs) > 0)
+        {
+            $this->weblinks = $childs;
+            echo $this->loadTemplate('childs');
+        }
+            ?>
+        </div>
+    <?php endif; ?>
+<?php endforeach; ?>
 <?php endif; ?>
