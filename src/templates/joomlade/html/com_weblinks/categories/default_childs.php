9eeab82e930e57add231dd229f4dbc43e73e768a
Tonino Gerns <info@livedesign24.de>
2015-09-24 14:06:02 +0200

    Aufbau grob. Noch inline-CSS, sollte später in die .less-Dateien übernommen werden.

--- src/templates/joomlade/html/com_weblinks/categories/default_childs.php
+++ src/templates/joomlade/html/com_weblinks/categories/default_childs.php
@@ -12,35 +12,38 @@ defined('_JEXEC') or die;
 $class = ' class="first"';
 $child_images = false; ?>
 
-<ul>
+<style>
+    img.linksammlung {width: 220px; height: 200px;}
+    .item-title-linksammlung {font-weight: bold; font-size: 16px;}
+    .item-title-linksammlung a{color: #000;}
+</style>
+
+<div class="row">
     <?php foreach ($this->weblinks as $id => $child) :
-
         $child_images = json_decode($child->images);
-
-        if (!isset($this->weblinks[$id + 1]))
-        {
-            $class = ' class="last"';
-        }
-        ?>
-        <li<?php echo $class; ?>>
-            <?php $class = ''; ?>
-            <?php if($child_images && $child_images->image_first != '') : ?>
-                <figure>
-                    <img src="<?php echo $child_images->image_first; ?>" alt="<?php echo $child_images->image_first_alt; ?>" />
-                    <?php if($child_images->image_first_caption != '') : ?>
-                        <figcaption><?php echo $this->escape($child_images->image_first_caption); ?></figcaption>
-                    <?php endif; ?>
-                </figure>
-            <?php endif; ?>
-            <span class="item-title"><a target="_blank" href="<?php echo JRoute::_(WeblinksHelperRoute::getWeblinkRoute($child->id, $child->catid)); ?>">
-                    <?php echo $this->escape($child->title); ?></a>
-            </span>
-            <?php if ($child->description) : ?>
-                <details class="category-desc">
-                    <?php echo JHtml::_('content.prepare', $child->description, '', 'com_weblinks.categories'); ?>
-                </details>
-            <?php endif; ?>
-        </li>
-
+            if (!isset($this->weblinks[$id + 1]))
+            {
+                $class = ' class="last"';
+            }
+            ?>
+        
+            <div class="col-xs-3">
+                <?php $class = ''; ?>
+                <?php if($child_images && $child_images->image_first != '') : ?>
+                    <figure>
+                        <img src="<?php echo $child_images->image_first; ?>" alt="<?php echo $child_images->image_first_alt; ?>" class="img-circle linksammlung"/>
+                        <?php if($child_images->image_first_caption != '') : ?>
+                            <figcaption><?php echo $this->escape($child_images->image_first_caption); ?></figcaption>
+                        <?php endif; ?>
+                    </figure>
+                <?php endif; ?>
+            </div>
+            <div class="col-xs-9">
+                <span class="item-title-linksammlung"><a target="_blank" href="<?php echo JRoute::_(WeblinksHelperRoute::getWeblinkRoute($child->id, $child->catid)); ?>"><?php echo $this->escape($child->title); ?></a>
+                </span>
+                <?php if ($child->description) : ?>    
+                    <?php echo JHtml::_('content.prepare', $child->description, '', 'com_weblinks.categories'); ?>                   
+                <?php endif; ?>
+            </div>
     <?php endforeach; ?>
-</ul>
+</div>
