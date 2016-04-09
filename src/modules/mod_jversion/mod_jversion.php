e5c40114118a8414d424b932ca6c09a1e127c3e4
David Neukirchen <david.neukirchen@gmail.com>
2016-04-09 11:35:05 +0200

    Added mod_jversion

---
+++ src/modules/mod_jversion/mod_jversion.php
@@ -0,0 +1,24 @@
+<?php
+
+defined('_JEXEC') or die;
+
+try {
+    // Require the module helper file
+    require_once __DIR__ . '/helper.php';
+
+    // Setup joomla cache
+    $helper = new ModJVersionHelper;
+    $cache  = JFactory::getCache();
+    $cache->setCaching(true);
+    $cache->setLifeTime($params->get('cache_time', 360));
+
+    // Get the latest version
+    $release = $cache->call(array($helper, 'latest'));
+
+    require JModuleHelper::getLayoutPath('mod_jversion', $params->get('layout', 'default'));
+} catch (Exception $e) {
+    JFactory::getApplication()->enqueueMessage(
+        'JVersion error: ' . $e->getMessage(), 'error'
+    );
+}
+
