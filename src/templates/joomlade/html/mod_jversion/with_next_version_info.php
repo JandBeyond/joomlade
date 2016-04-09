60a596571b7818cbf29f37c2356efc444eb501e8
David Neukirchen <david.neukirchen@gmail.com>
2016-04-09 18:46:09 +0200

    Added with_next_version_info override

--- src/templates/joomlade/html/mod_jversion/with_next_version_info.php
+++ src/templates/joomlade/html/mod_jversion/with_next_version_info.php
@@ -14,4 +14,8 @@
         <?php echo number_format($release->downloads, 0, ',', '.') ?> <?php echo JText::_('MOD_JVERSION_DOWNLOADS'); ?>
     </p>
 <?php endif; ?>
+<p class="name">N&auml;chste Version</p>
+<p>Beim Testen helfen</p>
+<p class="area-community"><a href="https://docs.joomla.org/Testing_Joomla!_patches/de" target="_blank" class="btn btn-primary download orange"><span class="fa fa-code-fork">&nbsp; </span>So geht's</a></p>
+
 
