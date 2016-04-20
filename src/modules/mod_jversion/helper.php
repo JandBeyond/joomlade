56154c77e61f7b2480c72ab00e63e12c98ed9bb0
David Neukirchen <david.neukirchen@gmail.com>
2016-04-20 21:17:13 +0200

    Get only the latest release (not pre-releases)

--- src/modules/mod_jversion/helper.php
+++ src/modules/mod_jversion/helper.php
@@ -7,26 +7,61 @@ defined('_JEXEC') or die;
  */
 class ModJVersionHelper
 {
+    protected $xmlParser;
+
     /**
-     * Get the latest joomla release
+     * Get the latest joomla release from github api
      */
-    public function latest()
+    public function latestRelease()
     {
-        $options = new Registry();
-        $options->def('userAgent', 'JGitHub/2.0');
+        // Get the latest version
+        $latestVersion = $this->latestVersion();
+
+        // Setup JHttp
+        $options       = new Registry();
+        $options->def('userAgent', 'JVersion/1.0');
         $transport = new JHttpTransportStream($options);
         $http      = new JHttp($options, $transport);
-        $response  = $http->get('https://api.github.com/repos/joomla/joomla-cms/releases/latest');
-        $release   = json_decode($response->body);
 
+        // Get the latest release from github api
+        $url      = 'https://api.github.com/repos/joomla/joomla-cms/releases/tags/' . $latestVersion;
+        $response = $http->get($url);
+        $release  = json_decode($response->body);
+
+        // Prepare the release
         $release->downloads = 0;
-        foreach ($release->assets as $asset) {
+        foreach ($release->assets as $asset)
+        {
             $release->downloads = $release->downloads + $asset->download_count;
-            if ($asset->content_type == 'application/zip' && strpos($asset->name, 'Full') !== false) {
+            if ($asset->content_type == 'application/zip' && strpos($asset->name, 'Full') !== false)
+            {
                 $release->downloadUrl = $asset->browser_download_url;
             }
         }
 
         return $release;
+    }
+
+    /**
+     * Gets the latest joomla version from the update server xml file
+     *
+     * @return string
+     */
+    protected function latestVersion()
+    {
+        $versions   = [];
+        $url        = 'https://update.joomla.org/core/sts/list_sts.xml';
+        $extensions = new SimpleXMLElement($url, 0, true);
+        foreach ($extensions as $extension)
+        {
+            $version = (string) $extension['version'];
+            if (!in_array($version, $versions))
+            {
+                $versions[] = $version;
+            }
+        }
+        usort($versions, 'version_compare');
+
+        return array_pop($versions);
     }
 }
