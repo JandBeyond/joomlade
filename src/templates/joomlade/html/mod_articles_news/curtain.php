62c74bf2856762c705649dc660c02ae7f97748d4
David Neukirchen <david.neukirchen@gmail.com>
2016-11-30 12:30:05 +0100

    Add article preview image

--- src/templates/joomlade/html/mod_articles_news/curtain.php
+++ src/templates/joomlade/html/mod_articles_news/curtain.php
@@ -8,32 +8,49 @@
  */
 defined('_JEXEC') or die; ?>
 <?php if (isset($list[0])) : ?>
+    <?php
+    $item   = $list[0];
+    $images = json_decode($item->images);
+    ?>
     <div class="curtain">
         <div class="curtain-wrapper">
             <input type="checkbox" checked>
             <div class="curtain-panel curtain-panel-left">
-                <?php if(!empty($list[0]->publish_up) && $list[0]->publish_up != JFactory::getDbo()->getNullDate()) : ?>
-                    Tür <?php echo JHtml::_('date', $list[0]->publish_up, 'j'); ?>
-                <?php else : ?>
-                    <?php echo $list[0]->title; ?>
+                <?php if (!empty($item->publish_up) && $item->publish_up != JFactory::getDbo()->getNullDate()) : ?>
+                    <?php echo JHtml::_('date', $item->publish_up, 'd', false)[0]; ?>
                 <?php endif; ?>
             </div>
             <div class="curtain-body">
-                <h3><?php echo $list[0]->title; ?></h3>
+                <div class="row">
+                    <?php if(!empty($images->image_intro)) : ?>
+                    <div class="col-md-3 text-center">
+                        <div class="pull-none item-image">
+                            <img src="<?php echo $images->image_intro; ?>" alt="<?php echo $images->image_intro_alt; ?>" class="img-rounded">
+                        </div>
+                    </div>
+                    <?php endif; ?>
+                    <div class="col-md-<?php echo (empty($images->image_intro)) ? '12' : '8'; ?>">
+                        <h4><?php echo $item->title; ?></h4>
 
-                <?php if (!$params->get('intro_only')) : ?>
-                    <?php echo $list[0]->afterDisplayTitle; ?>
-                <?php endif; ?>
+                        <?php if (!$params->get('intro_only')) : ?>
+                            <?php echo $item->afterDisplayTitle; ?>
+                        <?php endif; ?>
 
-                <?php echo $list[0]->beforeDisplayContent; ?>
+                        <?php echo $item->beforeDisplayContent; ?>
 
-                <?php echo $list[0]->introtext; ?>
+                        <?php echo $item->introtext; ?>
 
-                <?php if (isset($list[0]->link) && $list[0]->readmore != 0 && $params->get('readmore')) : ?>
-                    <?php echo '<a class="readmore" href="' . $list[0]->link . '">' . $list[0]->linkText . '</a>'; ?>
+                        <?php if (isset($item->link) && $item->readmore != 0 && $params->get('readmore')) : ?>
+                            <?php echo '<a class="readmore" href="' . $item->link . '">' . $item->linkText . '</a>'; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="curtain-panel curtain-panel-right">
+                <?php if (!empty($item->publish_up) && $item->publish_up != JFactory::getDbo()->getNullDate()) : ?>
+                    <?php echo JHtml::_('date', $item->publish_up, 'd', false)[1]; ?>
                 <?php endif; ?>
             </div>
-            <div class="curtain-panel curtain-panel-right">&nbsp;öffnen</div>
         </div>
     </div>
 <?php endif; ?>
