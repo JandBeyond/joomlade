a185c835c096cb200655574a9a7d30ad03b8faa2
David Neukirchen <david.neukirchen@gmail.com>
2017-05-19 06:26:00 +0200

    Update jversion

--- src/modules/mod_jversion/mod_jversion.php
+++ src/modules/mod_jversion/mod_jversion.php
@@ -2,23 +2,26 @@
 
 defined('_JEXEC') or die;
 
-try {
-    // Require the module helper file
-    require_once __DIR__ . '/helper.php';
+try
+{
+	// Require the module helper file
+	require_once __DIR__ . '/helper.php';
 
-    // Setup joomla cache
-    $helper = new ModJVersionHelper;
-    $cache  = JFactory::getCache('mod_jversion');
-    $cache->setCaching(true);
-    $cache->setLifeTime($params->get('cache_time', 360));
+	// Setup joomla cache
+	$helper = new ModJVersionHelper;
+	$cache  = JFactory::getCache('mod_jversion');
+	$cache->setCaching(true);
+	$cache->setLifeTime($params->get('cache_time', 360));
 
-    // Get the latest version
-    $release = $cache->call(array($helper, 'latestRelease'));
+	// Get the latest version
+	$release = $cache->call(array($helper, 'latestRelease'));
 
-    require JModuleHelper::getLayoutPath('mod_jversion', $params->get('layout', 'default'));
-} catch (Exception $e) {
-    JFactory::getApplication()->enqueueMessage(
-        'JVersion error: ' . $e->getMessage(), 'error'
-    );
+	require JModuleHelper::getLayoutPath('mod_jversion', $params->get('layout', 'default'));
+}
+catch (Exception $e)
+{
+	JFactory::getApplication()->enqueueMessage(
+		'JVersion error: ' . $e->getMessage(), 'error'
+	);
 }
 
